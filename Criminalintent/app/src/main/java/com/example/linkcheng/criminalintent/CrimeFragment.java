c4b1de9c3e80aa8d238982c7d82c11a7369e912d
zhenglong <zhenglong@zhenglongs-MacBook-Pro.local>
2017-05-12 19:31:36 +0800

    fragment in activity

--- Criminalintent/app/src/main/java/com/example/linkcheng/criminalintent/CrimeFragment.java
+++ Criminalintent/app/src/main/java/com/example/linkcheng/criminalintent/CrimeFragment.java
@@ -2,9 +2,12 @@ package com.example.linkcheng.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * Created by zhenglong on 2017/5/8.
@@ -12,6 +15,7 @@ import android.view.ViewGroup;
 
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
+    private EditText mTitleField;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,22 @@ public class CrimeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+
+        mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // ;
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setmTitle(s.toString());
+            }
+
+            public void afterTextChanged(Editable s) {
+                // ;
+            }
+        });
+
         return v;
     }
 }
